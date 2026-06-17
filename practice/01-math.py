32def35c525cf3426b5141e7b54997df734d7a33
Mini Gans <minigans@gmail.com>
2026-06-17 18:43:54 -0500

    Fix typo in subtract func name; make code PEP-compliant

--- practice/01-math.py
+++ practice/01-math.py
@@ -2,19 +2,23 @@ def add(x, y):
     result = x + y
     print(f"{x} + {y} = {result}")
 
-def substract(x, y):
+
+def subtract(x, y):
     result = x - y
     print(f"{x} - {y} = {result}")
+
 
 def multiply(x, y):
     result = x * y
     print(f"{x} * {y} = {result}")
 
+
 def divide(x, y):
     result = x / y
     print(f"{x} / {y} = {result}")
 
+
 add(1, 2)
-substract(10, 7)
+subtract(10, 7)
 multiply(4, 2)
 divide(10, 2)
