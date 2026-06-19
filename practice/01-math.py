76ee5bb64601085064373e9a066020006878619e
Kate Bopp <kateabopp@gmail.com>
2026-06-19 12:36:48 -0500

    Adding error handling to divide function in math

--- practice/01-math.py
+++ practice/01-math.py
@@ -14,11 +14,15 @@ def multiply(x, y):
 
 
 def divide(x, y):
-    result = x / y
-    print(f"{x} / {y} = {result}")
+    if y == 0:
+        print("Error. Dividend cannot be zero")
+    else:
+        result = x / y
+        print(f"{x} / {y} = {result}")
 
 
 add(1, 2)
 subtract(10, 7)
 multiply(4, 2)
 divide(10, 2)
+divide(15, 0)
